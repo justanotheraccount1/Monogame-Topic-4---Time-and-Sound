01cb047ad97327b496741c8c2142f8980d8cf75d
Sam Ahrens <339729139@GoTVDSB.ca>
2024-11-14 09:39:43 -0500

    Add project files.

---
+++ Monogame Topic 4 - Time and Sound/Game1.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Monogame_Topic_4___Time_and_Sound
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        Texture2D bombTexture, pliersTexture;
+        SpriteFont timeFont;
+        Rectangle bombRect;
+        float seconds;
+        SoundEffect explode;
+        MouseState mouseState;
+        Rectangle greenWire, redWire, pliersRect;
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 500;
+            _graphics.ApplyChanges();
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            bombRect = new Rectangle(50, 50, 700, 400);
+            greenWire = new Rectangle(500, 160, 100, 10);
+            redWire = new Rectangle(490, 190, 60, 10);
+            seconds = 10f;
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            bombTexture = Content.Load<Texture2D>("bomb");
+            pliersTexture = Content.Load<Texture2D>("pliers");
+            timeFont = Content.Load<SpriteFont>("TimeFont");
+            explode = Content.Load<SoundEffect>("explosion");
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            seconds -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            if (seconds <= 0)
+            {
+                seconds = 0;
+                explode.Play();
+            }
+            mouseState = Mouse.GetState();
+            this.Window.Title = $"x = {mouseState.X}, y = {mouseState.Y}";
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(bombTexture, bombRect, Color.White);
+            _spriteBatch.DrawString(timeFont, seconds.ToString("00.0"), new Vector2(270, 200), Color.Black);
+
+
+            // TODO: Add your drawing code here
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Monogame Topic 4 - Time and Sound/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Monogame_Topic_4___Time_and_Sound.Game1();
+game.Run();
